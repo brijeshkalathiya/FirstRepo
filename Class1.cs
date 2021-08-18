a36ba9cfb53e3569390ea9971361a215d264e99b
brijeshkalathiya <brijeshkalathiya8354@gmail.com>
2021-08-18 12:28:22 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static double add(double x,double y)
+        {
+            return x + y;
+        }
+
+        public static double subtract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
